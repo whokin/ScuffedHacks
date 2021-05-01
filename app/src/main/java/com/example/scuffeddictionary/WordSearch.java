698ce990458c9367528c5ce4b41020efff723a1a
Annie Yao <50132471+qyyao@users.noreply.github.com>
2021-05-01 10:32:23 -0700

    scrapes webster for definition

--- app/src/main/java/com/example/scuffeddictionary/WordSearch.java
+++ app/src/main/java/com/example/scuffeddictionary/WordSearch.java
@@ -3,10 +3,17 @@ package com.example.scuffeddictionary;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
 
 public class WordSearch extends AppCompatActivity {
 
@@ -27,15 +34,41 @@ public class WordSearch extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 word = searchWord.getText().toString();
-                definition = getDefinition(word);
-                Intent intent = new Intent(WordSearch.this, DefinitionDisplay.class);
-                intent.putExtra("DEFINITION", definition);
-                startActivity(intent);
+                Definition definition = new Definition(word);
+                definition.execute();
             }
         });
     }
 
-    String getDefinition(String word){
-        return word;
+
+
+    private class Definition extends AsyncTask<Void, Void, Void> {
+
+        String word;
+
+        public Definition(String word){
+            this.word = word;
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+
+            String url = "https://www.merriam-webster.com/dictionary/" + word + "/";
+
+            try {
+                Document doc = Jsoup.connect(url).timeout(6000).get();
+                Elements elements = doc.getElementsByClass("dtText");
+                String definition = elements.get(0).toString();
+
+                Intent intent = new Intent(WordSearch.this, DefinitionDisplay.class);
+                intent.putExtra("DEFINITION", definition);
+                startActivity(intent);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
     }
+
 }
