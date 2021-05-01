483075dcdf80322a602b2d4ff426778a40d8fb4f
Adamas8 <valerie.k.1480@gmail.com>
2021-05-01 10:42:44 -0700

    Update UI and remove status bar

--- app/src/main/java/com/example/scuffeddictionary/DefinitionDisplay.java
+++ app/src/main/java/com/example/scuffeddictionary/DefinitionDisplay.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 public class DefinitionDisplay extends AppCompatActivity {
@@ -11,6 +13,9 @@ public class DefinitionDisplay extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_definition_display);
 
         getSupportActionBar().hide();
