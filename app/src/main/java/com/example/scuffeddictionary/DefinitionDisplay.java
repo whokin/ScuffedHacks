20821750d4804e521898d03999e749dce4c1472c
Annie Yao <50132471+qyyao@users.noreply.github.com>
2021-05-01 09:10:50 -0700

    first activity passes word to second activity

--- app/src/main/java/com/example/scuffeddictionary/DefinitionDisplay.java
+++ app/src/main/java/com/example/scuffeddictionary/DefinitionDisplay.java
@@ -10,5 +10,7 @@ public class DefinitionDisplay extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_definition_display);
+
+
     }
 }
--- app/src/main/java/com/example/scuffeddictionary/WordSearch.java
+++ app/src/main/java/com/example/scuffeddictionary/WordSearch.java
@@ -2,6 +2,7 @@ package com.example.scuffeddictionary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -24,7 +25,8 @@ public class WordSearch extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 String word = searchWord.getText().toString();
-
+                Intent intent = new Intent(WordSearch.this, DefinitionDisplay.class);
+                intent.putExtra("SEARCH_WORD", word);
             }
         });
     }
