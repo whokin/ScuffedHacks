5a01def2a43f7a45f63c1cffa35ee2ee86f78ab8
Annie Yao <50132471+qyyao@users.noreply.github.com>
2021-05-01 09:06:52 -0700

    initialized first activity

---
+++ app/src/main/java/com/example/scuffeddictionary/WordSearch.java
@@ -0,0 +1,31 @@
+package com.example.scuffeddictionary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class WordSearch extends AppCompatActivity {
+
+    EditText searchWord;
+    Button search;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_word_search);
+
+        searchWord = findViewById(R.id.search_txt);
+        search = findViewById(R.id.search_button);
+
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String word = searchWord.getText().toString();
+
+            }
+        });
+    }
+}
